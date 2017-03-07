aaafe452fe9db6bd5480fcdf0b61e1b15d37856d
Arnold Lu <arnoldlu@qq.com>
2017-03-07 09:29:33 +0800

    add script for building
    Signed-off-by: Arnold Lu <arnoldlu@qq.com>

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# make
+# make parsertrace
+# make url_parser
+# make http_parser.o
+# gcc -Wall -Wextra -Werror -Wno-error=unused-but-set-variable -O3 http_parser.o demo.c -o demo
+# ./demo 
+
