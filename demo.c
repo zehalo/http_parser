d5888476aca69ca2c774ef50c6f72fcda722a218
Arnold Lu <arnoldlu@qq.com>
2017-03-07 14:42:25 +0800

    update demo.c
    Signed-off-by: Arnold Lu <arnoldlu@qq.com>

--- demo.c
+++ demo.c
@@ -1,9 +1,15 @@
-#include "http_parser.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/time.h>
+
+#include "http_parser.h"
 
 static http_parser *parser;
 
@@ -60,49 +66,111 @@ static http_parser_settings settings_null =
   ,.on_message_complete = on_message_complete
   };
 
-int
-main (void)
+#define BUFSIZZ 1024*8
+
+static double tminterval(struct timeval tmstart)
 {
-  const char *buf;
-  float start, end;
-  size_t parsed;
+	double ret = 0;
+	struct timeval now;
 
-  parser = malloc(sizeof(http_parser));
+	gettimeofday(&now, NULL);
 
-  buf = "GET http://admin.omsg.cn/uploadpic/2016121034000012.png HTTP/1.1\r\nHost: admin.omsg.cn\r\nAccept: */*\r\nConnection: Keep-Alive\r\n\r\n";
+	ret = ((now.tv_sec + now.tv_usec * 1e-6)
+	- (tmstart.tv_sec + tmstart.tv_usec * 1e-6));
+	tmstart = now;
 
-  start = (float)clock()/CLOCKS_PER_SEC;
+	return ret;
+}
 
- http_parser_init(parser, HTTP_REQUEST);
- parsed = http_parser_execute(parser, &settings_null, buf, strlen(buf));
+int main (void)
+{
+	const char *buf;
+	float start, end;
+	size_t parsed;
+	struct sockaddr_in address;
+	int client_sock;
+	int len,result;
+	int n,tmp;
+	char buffer[BUFSIZZ];
+	char *host="proxy.zte.com.cn";
+	short port = 80;
+	struct hostent *he;
+	struct in_addr **addr_list;
+	int i;
+	struct timeval tmstart;
 
-  end = (float)clock()/CLOCKS_PER_SEC;
+	#define REQUEST_STR "GET http://admin.omsg.cn/inuploadpic/2016121034000012.png HTTP/1.1\r\nHost: admin.omsg.cn\r\nAccept: */*\r\nConnection: Keep-Alive\r\n\r\n"
+
+	if ((he = gethostbyname(host)) == NULL) {  // get the host info
+		printf("gethostbyname error\n");
+		return 1;
+	}
+	printf("Official name is: %s\n", he->h_name);
+	printf("	  IP addresses: ");
+	addr_list = (struct in_addr **)he->h_addr_list;
+	for(i = 0; addr_list[i] != NULL; i++) {
+		printf("\t%s \n", inet_ntoa(*addr_list[i]));
+	}
+
+	client_sock=socket(AF_INET,SOCK_STREAM,0);
+	address.sin_addr.s_addr=inet_addr(inet_ntoa(*addr_list[0]));
+	address.sin_family=AF_INET;
+	address.sin_port=htons(port);
+	len=sizeof(address);
+	printf("@@@@@ line=%d time consumption=%lf\n", __LINE__, tminterval(tmstart));
+	result=connect(client_sock,(struct sockaddr *)&address,len);
+	if(result==-1){
+		printf("error!");
+		exit(-1);
+	}
+	printf("@@@@@ line=%d time consumption=%lf\n", __LINE__, tminterval(tmstart));
+	n=write(client_sock,REQUEST_STR, strlen(REQUEST_STR));
+	if(n<0){
+		printf("error write/n");
+	}
+	memset(buffer,0, sizeof(buffer));
+	printf("@@@@@ line=%d time consumption=%lf\n", __LINE__, tminterval(tmstart));
+	while(tmp=recv(client_sock,buffer,sizeof(buffer),0)){
+	if(tmp==-1) break;
+		printf("@@@@@ line=%d mbuf_len=%d time consumption=%lf, buffer=%s\n", __LINE__, tmp, tminterval(tmstart), buffer);
+	}
+
+	parser = malloc(sizeof(http_parser));
+
+	buf = "GET http://admin.omsg.cn/uploadpic/2016121034000012.png HTTP/1.1\r\nHost: admin.omsg.cn\r\nAccept: */*\r\nConnection: Keep-Alive\r\n\r\n";
+
+	start = (float)clock()/CLOCKS_PER_SEC;
+
+	http_parser_init(parser, HTTP_REQUEST);
+	parsed = http_parser_execute(parser, &settings_null, buf, strlen(buf));
+
+	end = (float)clock()/CLOCKS_PER_SEC;
 
 
-  buf="HTTP/1.1 200 OK\r\n"
-         "Date: Tue, 04 Aug 2009 07:59:32 GMT\r\n"
-         "Server: Apache\r\n"
-         "X-Powered-By: Servlet/2.5 JSP/2.1\r\n"
-         "Content-Type: text/xml; charset=utf-8\r\n"
-         "Connection: close\r\n"
-         "\r\n"
-         "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-         "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
-         "  <SOAP-ENV:Body>\n"
-         "    <SOAP-ENV:Fault>\n"
-         "       <faultcode>SOAP-ENV:Client</faultcode>\n"
-         "       <faultstring>Client Error</faultstring>\n"
-         "    </SOAP-ENV:Fault>\n"
-         "  </SOAP-ENV:Body>\n"
-         "</SOAP-ENV:Envelope>";
+	buf="HTTP/1.1 200 OK\r\n"
+	"Date: Tue, 04 Aug 2009 07:59:32 GMT\r\n"
+	"Server: Apache\r\n"
+	"X-Powered-By: Servlet/2.5 JSP/2.1\r\n"
+	"Content-Type: text/xml; charset=utf-8\r\n"
+	"Connection: close\r\n"
+	"\r\n"
+	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+	"<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+	"  <SOAP-ENV:Body>\n"
+	"    <SOAP-ENV:Fault>\n"
+	"       <faultcode>SOAP-ENV:Client</faultcode>\n"
+	"       <faultstring>Client Error</faultstring>\n"
+	"    </SOAP-ENV:Fault>\n"
+	"  </SOAP-ENV:Body>\n"
+	"</SOAP-ENV:Envelope>";
 
-  http_parser_init(parser, HTTP_RESPONSE);
-  parsed = http_parser_execute(parser, &settings_null, buf, strlen(buf));
+	http_parser_init(parser, HTTP_RESPONSE);
+	parsed = http_parser_execute(parser, &settings_null, buf, strlen(buf));
 
-  free(parser);
-  parser = NULL;
+	free(parser);
+	parser = NULL;
 
-  printf("Elapsed %f seconds.\n", (end - start));
+	printf("Elapsed %f seconds.\n", (end - start));
 
-  return 0;
+	return (EXIT_SUCCESS);
 }
